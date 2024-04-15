f752149953ee89ab09d3daf1c7a28f33cd94bc20
Edun Joshua Olubori <joshuaolubori@gmail.com>
2024-04-15 15:59:07 +0000

    first commit

---
+++ dags/transform_fixtures.py
@@ -0,0 +1,112 @@
+"DAG that runs a transformation on data in DuckDB using the Astro SDK."
+
+# --------------- #
+# PACKAGE IMPORTS #
+# --------------- #
+
+from airflow.decorators import dag
+from pendulum import datetime
+import pandas as pd
+
+# import tools from the Astro SDK
+from astro import sql as aql
+from astro.sql.table import Table
+
+# -------------------- #
+# Local module imports #
+# -------------------- #
+
+from include.global_variables import global_variables as gv
+
+from include.logic import apply_filtering_logic, won_last_5_matches
+
+
+def lenzi(df):
+    """Function to check for empty dataframe
+    https://stackoverflow.com/questions/19828822/how-do-i-check-if-a-pandas-dataframe-is-empty
+    """
+    return len(df.index) == 0
+
+# creating a dataframe to show if no fixtures obey filtering conditions
+default_df = pd.DataFrame(columns=['Date', 'HomeTeam', 'HomeScore', 'AwayScore', 'AwayTeam'])
+
+row_data = {
+    'Date': 'no',
+    'HomeTeam': 'fixtures',
+    'HomeScore': 'satisfies',
+    'AwayScore': 'condition',
+    'AwayTeam': ' yet'
+}
+default_df = default_df.append(row_data, ignore_index=True)
+
+
+# ----------------- #
+# Astro SDK Queries #
+# ----------------- #
+
+@aql.dataframe(pool="duckdb")
+def find_fixtures_c1(in_table: pd.DataFrame):
+    gv.task_log.info(in_table)
+
+    df = in_table
+    output_df = apply_filtering_logic(df)
+    
+    gv.task_log.info(output_df)
+    if lenzi(output_df) == True:
+        gv.task_log.info("df is empty")
+        return default_df
+
+    return output_df
+
+@aql.dataframe(pool="duckdb")
+def find_fixtures_c2(in_table: pd.DataFrame):
+    gv.task_log.info(in_table)
+
+    df = in_table
+
+    output_df = won_last_5_matches(df)
+    
+    gv.task_log.info(output_df)
+    if lenzi(output_df) == True:
+        gv.task_log.info("df is empty")
+        return default_df
+
+    return output_df
+
+
+# --- #
+# DAG #
+# --- #
+
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule=[gv.DS_DUCKDB_IN_FIXTURES],
+    catchup=False,
+    default_args=gv.default_args,
+    description="Runs transformations on fixtures data in DuckDB.",
+    tags=["transformation"],
+)
+def e_transform_fixtures():
+
+
+    find_fixtures_c1(
+        in_table=Table(
+            name=gv.FIXTURES_IN_TABLE_NAME, conn_id=gv.CONN_ID_DUCKDB
+        ),
+        output_table=Table(
+            name=gv.REPORTING_TABLE_NAME_1, conn_id=gv.CONN_ID_DUCKDB
+        ),
+    )
+
+    find_fixtures_c2(
+        in_table=Table(
+            name=gv.FIXTURES_IN_TABLE_NAME, conn_id=gv.CONN_ID_DUCKDB
+        ),
+        output_table=Table(
+            name=gv.REPORTING_TABLE_NAME_2, conn_id=gv.CONN_ID_DUCKDB
+        ),
+    )
+
+
+e_transform_fixtures()
---
+++ include/global_variables/global_variables.py
@@ -0,0 +1,91 @@
+# --------------- #
+# PACKAGE IMPORTS #
+# --------------- #
+
+from airflow import Dataset
+from airflow.models import Variable
+import logging
+import os
+from minio import Minio
+from pendulum import duration
+import json
+
+# ----------------------- #
+# Configuration variables #
+# ----------------------- #
+
+# MinIO connection config
+MINIO_ACCESS_KEY = "minioadmin"
+MINIO_SECRET_KEY = "minioadmin"
+MINIO_IP = "host.docker.internal:9000"
+FIXTURES_BUCKET_NAME = "fixtures"
+# ARCHIVE_BUCKET_NAME = "archive"
+
+# Source file path climate data
+FIXTURES_DATA_PATH = (
+    f"{os.environ['AIRFLOW_HOME']}/include/fixtures_data/all_fixtures_combined.csv"
+)
+
+FIXTURES_DATA_FOLDER = (
+    f"{os.environ['AIRFLOW_HOME']}/include/fixtures_data/"
+)
+
+RESULTS_DATA_PATH = (
+    f"{os.environ['AIRFLOW_HOME']}/include/results/"
+)
+
+# DuckDB config
+DUCKDB_INSTANCE_NAME = json.loads(os.environ["AIRFLOW_CONN_DUCKDB_DEFAULT"])["host"]
+FIXTURES_IN_TABLE_NAME = "in_fixtures"
+REPORTING_TABLE_NAME_1 = "reporting_table_1"
+REPORTING_TABLE_NAME_2 = "reporting_table_2"
+CONN_ID_DUCKDB = "duckdb_default"
+
+# Datasets
+DS_FIXTURES_DATA_MINIO = Dataset(f"minio://{FIXTURES_BUCKET_NAME}")
+DS_DUCKDB_IN_FIXTURES = Dataset(f"duckdb://{FIXTURES_IN_TABLE_NAME}")
+DS_DUCKDB_REPORTING = Dataset("duckdb://reporting")
+DS_START = Dataset("start")
+DS_INGEST = Dataset("ingest")
+
+# API KEYS
+
+# API_ENDPOINT = Variable.get("API_ENDPOINT")
+# API_KEY = Variable.get("API_KEY")
+# API_HOST = Variable.get("API_HOST")
+
+API_ENDPOINT = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
+API_KEY = "8feeed635dmshbab9080dc4f248bp112d75jsn9661eab3597b"
+API_HOST = "api-football-v1.p.rapidapi.com"
+
+# LEAGUE IDS
+LEAGUE_IDS = [39, 40,
+              41, 42, 43, 50, 51, 59, 60,
+             78, 79, 83, 84, 85, 86, 61, 62, 135, 136,
+             88, 89, 106, 107,  144, 283, 94, 95, 96,
+             140, 141, 142, 876, 435, 436, 210, 318, 345,
+             203, 204, 271, 272, 235, 373, 307, 308, 301, 303,
+             207, 208, 179, 180, 183, 184,
+            305, 233, 290, 218, 219, 419, 172,
+             119, 120, 236, 288, 128, 129, 134, 71, 72, 383, 382
+              ]
+
+
+
+# get Airflow task logger
+task_log = logging.getLogger("airflow.task")
+
+# DAG default arguments
+default_args = {
+    "owner": "Edun Joshua",
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delay": duration(minutes=1),
+}
+
+
+# utility functions
+def get_minio_client():
+    client = Minio(MINIO_IP, MINIO_ACCESS_KEY, MINIO_SECRET_KEY, secure=False)
+
+    return client
